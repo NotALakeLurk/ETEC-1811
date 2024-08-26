5cef2ee93412ee0aaf73097f0bdb395c05fa821f
John Reed <reallyjohnreed+git@gmail.com>
2024-08-26 18:13:40 -0400

    initial commit

---
+++ python/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A simple flake that provides a development environment for Shawnee's ETEC 1811 course.";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+        ];
+
+        packages = with pkgs; [
+          thonny
+          python3
+        ];
+      };
+    }
+  );
+}
